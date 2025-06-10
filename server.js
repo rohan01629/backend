28baf946b917fecf0bc9bb7d299d2c298e489f7d
Rohan Kumar Singh <128318601+rohan01629@users.noreply.github.com>
2025-06-11 01:22:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());  // For parsing application/json
 
 // Enable CORS from your React frontend URL
 app.use(cors({
-  origin: "https://frontend-xi-black-rhhyyxri8m.vercel.app",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
